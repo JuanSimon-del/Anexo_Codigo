a63e1697e27e59fcf6523d095af4a071e3e1d370
AguchoD <agu2darelli@gmail.com>
2026-06-02 14:20:55 -0300

    Front con Correcciones visuales

--- frontend/src/components/DropdownTrabajadores.jsx
+++ frontend/src/components/DropdownTrabajadores.jsx
@@ -3,24 +3,32 @@ import Dropdown from "react-bootstrap/Dropdown";
 function DropdownTrabajadores({ etapa, valor, onChange }) {
   return (
     <>
-      <section className="d-flex w-100 my-3">
+      <section className="d-flex w-100 my-3 align-items-center justify-content-center">
+        {/* Input que actúa como texto de guía fija + muestra el valor actual */}
         <input
-          type="number"
-          placeholder="Ej: 5 trabajadores"
-          value={valor}
-          onChange={(e) => onChange(parseInt(e.target.value) || 0)}
-          className="ms-5 px-3 border border-1 rounded-2 input bg-light me-2"
-          min="1"
+          type="text"
+          readOnly
+          value={`Trabajadores Etapa ${etapa}: ${valor}`}
+          className="ms-5 px-3 border border-1 rounded-2 input bg-light me-2 text-center"
+          style={{ width: "220px", fontWeight: "500" }}
         />
-        <Dropdown>
-          <Dropdown.Toggle variant="success" id={`dropdown-${etapa}`}>
-            {valor || "Seleccionar"}
+        
+        {/* Desplegable para seleccionar la cantidad */}
+        <Dropdown onSelect={(e) => onChange(Number(e))}>
+          <Dropdown.Toggle
+            variant="secondary"
+            id={`dropdown-${etapa}`}
+            className="px-3"
+          >
+            {/* Dejamos el toggle limpio o con el valor según prefieras, variant secondary le da el gris de la foto */}
           </Dropdown.Toggle>
+
           <Dropdown.Menu>
             {[1, 2, 3, 4, 5].map((num) => (
-              <Dropdown.Item
-                key={num}
-                onClick={() => onChange(num)}
+              <Dropdown.Item 
+                key={num} 
+                eventKey={num}
+                active={valor === num}
               >
                 {num}
               </Dropdown.Item>
